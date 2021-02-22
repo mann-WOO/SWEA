1ebc087376e73684190ca51ad5e3ea7255b9dd1b
mann_windows <6513032@gmail.com>
2021-02-23 08:58:43 +0900

    add 10965, 2005 solutions

--- 10965_제곱수만들기/sol1.py
+++ 10965_제곱수만들기/sol1.py
@@ -3,8 +3,19 @@ sys.stdin = open("input.txt")
 
 T = int(input())
 for tc in range(1, T+1):
+    # 거듭제곱수로 만들 수 A
     A = int(input())
+    # 2부터 A까지의 수 i에 대해
+    for i in range(2, A+1):
+        if A < i ** 2:
+            break
+        if A % i ** 2 == 0:
+            A = A // i ** 2
 
-    
-    print("#{} ".format(tc, ))
+    current = 2
+    while A < current ** 2:
+        if A % (current ** 2) == 0 :
+            A = A // (current ** 2)
+
+    print("#{} {}".format(tc, A))
 
--- 2005_파스칼삼각형/sol1.py
+++ 2005_파스칼삼각형/sol1.py
@@ -3,7 +3,29 @@ import sys
 sys.stdin = open("input.txt")
 
 T = int(input())
-
 for tc in range(1, T + 1):
-    print("#{} ".format(tc, ))
+    N = int(input())
+    # 첫번째 줄, 첫 번째 스택은 완성되어 있음.
+    stack = [1]
+    # 완성된 한 줄을 담을 row
+    row = []
+    # 결과 row들을 담을 result
+    result = [[1]]
+    # 두 번째 줄부터 완성시켜 추가한다.
+    for i in range(1, N):
+        # 줄의 길이 설정: 2, 3, 4 ...
+        row = [0] * (i + 1)
+        # n번째 줄이라면, 그 위의 줄의 원소 개수인 n-1만큼 반복한다.
+        for j in range(i):
+            tmp = stack.pop()
+            row[j] += tmp
+            row[j + 1] += tmp
+        # 완성된 줄을 결과에 추가
+        result.append(list(row))
+        # 스택을 이번에 완성시킨 줄로 갱신
+        stack = list(row)
+
+    print("#{} ".format(tc))
+    for row in result:
+        print(' '.join([str(num) for num in row]))
 
